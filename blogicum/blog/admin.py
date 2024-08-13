c5f71e20ea1dd588f639f819bbf306e6600364b0
Konstantin Makeev <kostia1500@gmail.com>
2024-08-13 18:59:14 +0300

    Codestylee fix.

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -25,23 +25,6 @@ class LocationAdmin(admin.ModelAdmin):
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
 
-    @admin.display(description='Изображение')
-    def image_tag(self, obj):
-        if obj.image:
-            return mark_safe(
-                '<img src="/%s" width="150" height="150" />' % obj.image
-            )
-        else:
-            return "Нет изображения"
-
-    def get_queryset(self, request):
-        queryset = super().get_queryset(request)
-        return queryset.with_comment_count()
-
-    @admin.display(description='Количество комментариев')
-    def get_comment_count(self, obj):
-        return obj.comment_count
-
     common_list = (
         'title',
         'is_published',
@@ -59,6 +42,23 @@ class PostAdmin(admin.ModelAdmin):
         + ('get_comment_count',)
     )
     list_editable = common_list
+
+    @admin.display(description='Изображение')
+    def image_tag(self, obj):
+        if obj.image:
+            return mark_safe(
+                '<img src="/%s" width="150" height="150" />' % obj.image
+            )
+        else:
+            return 'Нет изображения'
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.with_comment_count()
+
+    @admin.display(description='Количество комментариев')
+    def get_comment_count(self, obj):
+        return obj.comment_count
 
 
 @admin.register(Category)
